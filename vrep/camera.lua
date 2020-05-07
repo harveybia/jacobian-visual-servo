c07ca633b6f1c103e74100ee8949e15c19c1a83b
harveybia <harvey.shi@yahoo.com>
2020-05-06 23:25:52 -0400

    added pose and tf publish for vrep

--- vrep/camera.lua
+++ vrep/camera.lua
@@ -20,6 +20,8 @@ function sysCall_init()
         blaserVideoPub=simROS.advertise('/snake_cam/image_color', 'sensor_msgs/Image')
         -- treat uint8 arrays as strings (much faster, tables/arrays are kind of slow in Lua)
         simROS.publisherTreatUInt8ArrayAsString(blaserVideoPub)
+
+        posePub = simROS.advertise('/snake_arm/pose', 'geometry_msgs/Pose')
     end
 end
 
@@ -43,6 +45,17 @@ function sysCall_sensing()
         d['data']=data
         simROS.publish(blaserVideoPub,d)
     end
+
+    -- Publish the end effector transform and pose
+    if rosInterfacePresent then
+        base_handle=sim.getObjectHandle('snake_arm')
+        tf_base_link_ee=getTransformStamped(blaserCamHandle,'ee_link',base_handle,'base_link')
+        -- print(tf_base_link_ee)
+        simROS.sendTransform(tf_base_link_ee)
+
+        pose_base_link_ee=getPose(blaserCamHandle,base_handle)
+        simROS.publish(posePub,pose_base_link_ee)
+    end
 end
 
 function sysCall_cleanup()
@@ -51,6 +64,34 @@ function sysCall_cleanup()
         simROS.shutdownPublisher(blaserVideoPub)
         sim.addStatusbarMessage('Cleaned up: Snake Camera')
     end
+end
+
+function getTransformStamped(objHandle,name,relTo,relToName)
+    -- This function retrieves the stamped transform for a specific object
+    t = sim.getSystemTime()
+    p = sim.getObjectPosition(objHandle,relTo)
+    o = sim.getObjectQuaternion(objHandle,relTo)
+	return {
+        header={
+            stamp=t,
+            frame_id=relToName
+        },
+        child_frame_id=name,
+        transform={
+            translation={x=p[1],y=p[2],z=p[3]},
+            rotation={x=o[1],y=o[2],z=o[3],w=o[4]}
+        }
+    }
+end
+
+function getPose(objectHandle, relTo)
+    -- This function get the object pose at ROS format geometry_msgs/Pose
+    p = sim.getObjectPosition(objectHandle,relTo)
+    o = sim.getObjectQuaternion(objectHandle,relTo)
+    return {
+        position={x=p[1],y=p[2],z=p[3]},
+        orientation={x=o[1],y=o[2],z=o[3],w=o[4]}
+    }
 end
 
 -- See the user manual or the available code snippets for additional callback functions and details
