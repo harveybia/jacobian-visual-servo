952f5a5c525408134ddbd003a6eedba135add994
harveybia <harvey.shi@yahoo.com>
2020-05-07 00:07:06 -0400

    added joint angle publisher

--- vrep/snake_arm.lua
+++ vrep/snake_arm.lua
@@ -54,15 +54,35 @@ function sysCall_threadmain()
     joints_v={-1,-1,-1,-1}
     
     jointHandles={-1,-1,-1,-1,-1,-1,-1,-1,-1}
-    jointHandles[1]=sim.getObjectHandle('snake_joint_cam')
-    jointHandles[2]=sim.getObjectHandle('snake_joint_v1')
-    jointHandles[3]=sim.getObjectHandle('snake_joint_h1')
-    jointHandles[4]=sim.getObjectHandle('snake_joint_v2')
-    jointHandles[5]=sim.getObjectHandle('snake_joint_h2')
-    jointHandles[6]=sim.getObjectHandle('snake_joint_v3')
-    jointHandles[7]=sim.getObjectHandle('snake_joint_h3')
-    jointHandles[8]=sim.getObjectHandle('snake_joint_v4')
-    jointHandles[9]=sim.getObjectHandle('snake_joint_h4')
+    backlashHandles={-1,-1,-1,-1,-1,-1,-1,-1,-1}
+    jointNames={'snake_joint_cam','snake_joint_v1','snake_joint_h1',
+                'snake_joint_v2','snake_joint_h2','snake_joint_v3',
+                'snake_joint_h3','snake_joint_v4','snake_joint_h4'}
+    jointHandles[1]=sim.getObjectHandle(jointNames[1])
+    jointHandles[2]=sim.getObjectHandle(jointNames[2])
+    jointHandles[3]=sim.getObjectHandle(jointNames[3])
+    jointHandles[4]=sim.getObjectHandle(jointNames[4])
+    jointHandles[5]=sim.getObjectHandle(jointNames[5])
+    jointHandles[6]=sim.getObjectHandle(jointNames[6])
+    jointHandles[7]=sim.getObjectHandle(jointNames[7])
+    jointHandles[8]=sim.getObjectHandle(jointNames[8])
+    jointHandles[9]=sim.getObjectHandle(jointNames[9])
+
+    backlashHandles[1]=sim.getObjectHandle(jointNames[1].."_backlash")
+    backlashHandles[2]=sim.getObjectHandle(jointNames[2].."_backlash")
+    backlashHandles[3]=sim.getObjectHandle(jointNames[3].."_backlash")
+    backlashHandles[4]=sim.getObjectHandle(jointNames[4].."_backlash")
+    backlashHandles[5]=sim.getObjectHandle(jointNames[5].."_backlash")
+    backlashHandles[6]=sim.getObjectHandle(jointNames[6].."_backlash")
+    backlashHandles[7]=sim.getObjectHandle(jointNames[7].."_backlash")
+    backlashHandles[8]=sim.getObjectHandle(jointNames[8].."_backlash")
+    backlashHandles[9]=sim.getObjectHandle(jointNames[9].."_backlash")
+
+    rosInterfacePresent = sim.isPluginLoaded('ROSInterface')
+    if rosInterfacePresent then
+        jStatePub = simROS.advertise(
+            '/snake_arm/joint_states', 'sensor_msgs/JointState')
+    end
     
     for i=1,4,1 do
         joints_h[i]=sim.getObjectHandle('snake_joint_h'..(i))
@@ -73,6 +93,7 @@ function sysCall_threadmain()
     end
 
     t=0
+    seq=0
 
     while sim.getSimulationState()~=sim.simulation_advancing_abouttostop do
         while #receivedMessages>0 do
@@ -81,6 +102,11 @@ function sysCall_threadmain()
             -- do something with the msg
             process_msg(msg)
         end
+
+        if rosInterfacePresent then
+            publishJointStates()
+        end
+
         sim.switchThread()
     end
 
@@ -88,4 +114,26 @@ end
 
 function sysCall_cleanup()
     -- Put some clean-up code here
+end
+
+function publishJointStates()
+    -- This joint states feedback simulates actual joint angle
+    -- read off from joint encoders
+    actual_joint_states={0,0,0,0,0,0,0,0,0}
+    for i=1,9,1 do
+        jHandle=jointHandles[i]
+        bHandle=backlashHandles[i]
+        actual_joint_states[i]=
+            sim.getJointPosition(jHandle)+sim.getJointPosition(bHandle)
+    end
+
+    d={}
+    d['header']={seq=0,stamp=simROS.getTime(), frame_id="a"}
+    d['name']=jointNames
+    d['position']=actual_joint_states
+    d['velocity']={0,0,0,0,0,0,0,0,0}
+    d['effort']={0,0,0,0,0,0,0,0,0}
+    simROS.publish(jStatePub,d)
+
+    seq=seq+1
 end
